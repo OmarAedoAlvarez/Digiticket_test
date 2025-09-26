564d06176817405baaf1068466a2594be4877f81
Anibal <anibal.25.gonzales@gmail.com>
2025-09-26 17:26:17 -0500

    feat(app): add Spring Boot main application entrypoint

--- Backend/DigiTicket/src/main/java/com/digiticket/DigiTicketApplication.java
+++ Backend/DigiTicket/src/main/java/com/digiticket/DigiTicketApplication.java
@@ -1,4 +1,11 @@
 package com.digiticket;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DigiTicketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DigiTicketApplication.class, args);
+    }
 }
