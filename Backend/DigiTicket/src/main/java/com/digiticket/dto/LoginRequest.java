53964fbb7f2b3b38241d0975dbe142699982d949
Anibal <anibal.25.gonzales@gmail.com>
2025-09-30 22:23:00 -0500

    style(login): eliminar comentarios en el LoginRequest

--- Backend/DigiTicket/src/main/java/com/digiticket/dto/LoginRequest.java
+++ Backend/DigiTicket/src/main/java/com/digiticket/dto/LoginRequest.java
@@ -4,10 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @Email(message = "Email no válido")
-        @NotBlank(message = "El email es obligatorio")
-        String email,
-
-        @NotBlank(message = "La contraseña es obligatoria")
-        String password
+        @NotBlank @Email String email,
+        @NotBlank String password
 ) {}
