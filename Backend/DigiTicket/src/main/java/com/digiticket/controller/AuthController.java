996b4c73736288d8342850c0f558fc5e5789052b
Anibal <anibal.25.gonzales@gmail.com>
2025-09-30 22:20:49 -0500

    refactor(auth): añadir endpoint /register en AuthController

--- Backend/DigiTicket/src/main/java/com/digiticket/controller/AuthController.java
+++ Backend/DigiTicket/src/main/java/com/digiticket/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.digiticket.controller;
 
+import com.digiticket.dto.ClientRegisterRequest;
+import com.digiticket.dto.ClientRegisterResponse;
 import com.digiticket.dto.LoginResponse;
 import com.digiticket.dto.LoginRequest;
 import com.digiticket.service.AuthService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +21,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest req) {
         return ResponseEntity.ok(authService.login(req));
+    }
+
+    @PostMapping("/register")
+    public  ResponseEntity<ClientRegisterResponse>  registerClient(@Valid @RequestBody ClientRegisterRequest req) {
+        return  ResponseEntity.status(HttpStatus.CREATED).body(authService.registerClient(req));
     }
 }
