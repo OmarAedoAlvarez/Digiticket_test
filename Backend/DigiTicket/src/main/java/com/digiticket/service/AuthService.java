d049846301f0585d6d8fa4390f0e42fe918f2e52
Anibal <anibal.25.gonzales@gmail.com>
2025-09-30 22:21:06 -0500

    refactor(auth): implementar lógica de registro en AuthServiceImpl

--- Backend/DigiTicket/src/main/java/com/digiticket/service/AuthService.java
+++ Backend/DigiTicket/src/main/java/com/digiticket/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.digiticket.service;
 
+import com.digiticket.dto.ClientRegisterRequest;
+import com.digiticket.dto.ClientRegisterResponse;
 import com.digiticket.dto.LoginRequest;
 import com.digiticket.dto.LoginResponse;
 
 public interface AuthService {
     LoginResponse login(LoginRequest request);
+    ClientRegisterResponse registerClient(ClientRegisterRequest request);
 }
